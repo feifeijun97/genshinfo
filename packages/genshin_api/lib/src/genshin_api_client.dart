1865084ccc23f56a7d7588cc6ad8596f2a4f1f32
Tan Jun Hong <junhoongtan@gmail.com>
2022-02-04 19:36:37 +0800

    api client to retrieve data

---
+++ packages/genshin_api/lib/src/genshin_api_client.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:genshin_api/src/models/character.dart';
+import 'package:http/http.dart' as http;
+
+/// Exception throw when the characters/{name} is not found
+class CharacterNotFound implements Exception {}
+
+/// Exception throw when there is no character return from 'characters/all'
+class EmptyCharacterList implements Exception {}
+
+class GenshinApiClient {
+  static const _baseUrl = "api.genshin.dev";
+  final http.Client _httpClient;
+
+  GenshinApiClient({http.Client? httpClient})
+      : _httpClient = httpClient ?? http.Client();
+
+  Future<List<Character>> getAllCharacters() async {
+    // List<Character> characterList = List<Character>[];
+    var url = Uri.parse("$_baseUrl/characters/all");
+
+    var response = await _httpClient.get(url);
+
+    if (kDebugMode) {
+      print("Response status: ${response.statusCode}");
+      print("Response body: ${response.body}");
+    }
+
+    final Iterable charactersList = jsonDecode(response.body);
+
+    if (charactersList.isEmpty) {
+      throw EmptyCharacterList();
+    }
+    
+    return List<Character>.from(
+        charactersList.map((e) => Character.fromJson(e)));
+  }
+}
